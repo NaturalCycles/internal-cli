e8ac86da0d95cb98fe5299592943921761de2a62
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-07-09 11:19:40 +0200

    chore: progress

--- src/bin/nc-test-cli.ts
+++ src/bin/nc-test-cli.ts
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
 
 import { runScript } from '@naturalcycles/nodejs-lib/dist/script'
+import * as inquirer from 'inquirer'
+
+const BASES = {
+  COUNTRIES: 'applVrIQSYcnBG8tZ',
+  LALALA: 'lalala',
+}
 
 runScript(async () => {
-  console.log('Good morning, Cyclers!')
+  // console.log('Good morning, Cyclers!')
+
+  const answers = await inquirer.prompt([
+    {
+      name: 'question1',
+      // input: 'question1',
+    },
+    {
+      name: 'Airtable BASE',
+      type: 'list',
+      choices: Object.keys(BASES),
+    },
+
+    // Q of key
+    // Q of en-US copy
+  ])
+
+  console.log({answers})
+
+  // validation
+  // use airrable-lib to insert a row
 })
